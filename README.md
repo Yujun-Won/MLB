3e1be3f1b16d8575c0b3554093b50488c20612d9
Yujun Won <yujunwon27@naver.com>
2023-08-04 10:35:00 +0900

    feat: 선수 개인 연봉 crawler 구현 및 데이터셋 업데이트

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MLB Project
+
+### 참고자료
+1. [[라인맨 포지션 평가] 웹 페이지 테이블 크롤링](https://velog.io/@eunsuh/%EB%9D%BC%EC%9D%B8%EB%A7%A8-%ED%8F%AC%EC%A7%80%EC%85%98-%ED%8F%89%EA%B0%80-%EC%9B%B9-%ED%8E%98%EC%9D%B4%EC%A7%80-%ED%85%8C%EC%9D%B4%EB%B8%94-%ED%81%AC%EB%A1%A4%EB%A7%81)
---
+++ player_salary_crawler.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+# ChromeWebDriver 설정
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+categories = ['batters', 'pitching']
+
+for category in categories:
+    driver.get(f"https://www.spotrac.com/mlb/rankings/2022/salary/{category}/")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "table")))
+
+    # table의 XPATH를 설정하고, table의 요소들을 차례대로 find_element()
+    table = driver.find_element(By.XPATH,
+                                "/html/body/div[1]/div[2]/div[1]/div/div/div[1]/div/div[3]/div/table")
+    tbody = table.find_element(By.TAG_NAME, "tbody")
+
+    data = []  # DataFrame을 위한 데이터 리스트 초기화
+
+    rows = tbody.find_elements(By.TAG_NAME, "tr")
+    for i, value in enumerate(rows, start=1):
+        name = value.find_elements(By.TAG_NAME, "td")[1].text.split("\n")[0]
+        team = value.find_elements(By.TAG_NAME, "td")[1].text.split("\n")[1]
+        position = value.find_elements(By.TAG_NAME, "td")[2].text
+        age = value.find_elements(By.TAG_NAME, "td")[3].text
+
+        # batter와 pitcher에 따라 다른 요소를 구분 (batter: bat / pitcher: throw)
+        bat_throw = value.find_elements(By.TAG_NAME, "td")[4].text
+        salary = value.find_elements(By.TAG_NAME, "td")[-1].text.replace("$", "").replace(",", "")
+
+        data.append([i, name, team, position, age, bat_throw, salary])
+
+    # DataFrame 생성 및 CSV 저장
+    if category == 'batters':
+        df = pd.DataFrame(data,
+                          columns=["No", "Name", "Team", "Position", "Age", "Bats", "Salary"])
+        df.to_csv("./dataset_2022/batter-salary.csv", index=False)
+    else:
+        df = pd.DataFrame(data,
+                          columns=["No", "Name", "Team", "Position", "Age", "Throws", "Salary"])
+        df.to_csv("./dataset_2022/pitcher-salary.csv", index=False)
+
+# WebDriver 종료
+driver.quit()
